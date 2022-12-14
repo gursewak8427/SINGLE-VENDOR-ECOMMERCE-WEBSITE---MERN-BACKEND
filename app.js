e80302c2e0809ea5fc27b547450f06cef228e93c
gursewak8427 <gursewaksaggu2043@gmail.com>
2022-12-15 01:25:39 +0530

    cors

--- app.js
+++ app.js
@@ -7,7 +7,25 @@ const cors = require("cors");
 const connectDB = require('./config/db');
 connectDB();
 
-app.use(cors({ origin: true, credentials: true }));
+var allowedOrigins = ['http://localhost:3000',
+    'https://single-vendor-ecommerce-website-mern.vercel.app',
+    'https://ecommerce-backend-fffe.onrender.com'];
+
+app.use(cors({
+    origin: function (origin, callback) {
+        // allow requests with no origin 
+        // (like mobile apps or curl requests)
+        if (!origin) return callback(null, true);
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'The CORS policy for this site does not ' +
+                'allow access from the specified Origin.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
+}));
+// app.use(cors({ origin: true, credentials: true }));
+
 app.use(express.json({ extended: false }));
 app.use("/files", express.static("files")); //The folder where the files or uploads are located
 
